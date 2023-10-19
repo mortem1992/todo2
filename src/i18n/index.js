45d3f4b65c6abe8f22418ea94be17e5bb3314bd6
mortem <gurin.ivan@icloud.com>
2023-10-19 21:03:38 +0500

    tododo

---
+++ src/i18n/index.js
@@ -0,0 +1,7 @@
+import ru from './ru.json'
+import uz from './uz.json'
+
+export const languages = {
+    ru,
+    uz
+}
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/scss/main.scss'
+
+import { languages } from './i18n'
+import { createI18n } from 'vue-i18n'
+const app = createApp(App)
+const i18n = createI18n({
+    legacy: true,
+    locale: 'ru',
+    messages: languages
+})
+app.use(i18n)
+app.mount('#app')
